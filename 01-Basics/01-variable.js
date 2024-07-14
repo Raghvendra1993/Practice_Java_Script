b656aa3972dd39e68c48fbd839d1dc920812e052
Raghvendra <raghavmdbn@gmail.com>
2024-07-15 04:05:42 +0530

    01 Variable

---
+++ 01-Basics/01-variable.js
@@ -0,0 +1,9 @@
+const id = 54321;
+let account_Name = "Raghvendra";
+var city = "Delhi";
+// id = 23456; constant value can not be changed
+account_Name = "Raghvendra Singh";
+city = "Noida";
+state = "UP";
+let coutry;
+console.table([id, account_Name, city, state, coutry]);
